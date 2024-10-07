acd6f2df60ee7dc8a079e9df69b65ed7fc2dbbee
Laura Marcos <lauramarcosmo@gmail.com>
2024-10-07 13:43:54 +0200

    suma lau

---
+++ ejerciciolau.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class ejerciciolau
+{
+	public static void main (String[]args)
+	{
+		num1=7;
+		num2=3
+		int suma;
+		suma=num1+num2;
+		System.out.println("La suma es "+suma);
+	}
+}
